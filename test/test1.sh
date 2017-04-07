f753733dfb1cd766a80bd51568521a2f29184782
Marcus Breese <marcus.breese@ucsf.edu>
2017-04-06 23:00:55 -0400

    removed -wt option (not used), updated docs, added a few test scripts

---
+++ test/test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBS -name namedjob
+#SBS -hold
+#SBS -procs 4
+#SBS -mail mbreese
+hostname
+date
+sleep 20
+date
---
+++ test/test2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBS -name namedjob
+#SBS -hold
+#SBS -procs 2
+#SBS -mail mbreese
+#SBS -afterok 1
+hostname
+date
+sleep 20
+date
